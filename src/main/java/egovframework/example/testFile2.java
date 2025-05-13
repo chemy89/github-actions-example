a5f6856fbce1f3d1f931b87185e30b96d57acbf1
jeongsejong <chemy@kakao.com>
2025-05-13 16:48:00 +0900

    test

---
+++ src/main/java/egovframework/example/testFile2.java
@@ -0,0 +1,12 @@
+public class testFile2 {
+    
+    public void testMethod() {
+        // This is a test method
+        System.out.println("Hello, World!");
+    }
+    
+    public static void main(String[] args) {
+        testFile2 test = new testFile2();
+        test.testMethod();
+    }
+}
